5923cadbb187ea6c88e7c42c318ee0179ac06fc4
strato <ebourbou@gmail.com>
2020-06-27 15:51:45 +0200

    importance in form

--- public/js/handlebars.js
+++ public/js/handlebars.js
@@ -9,9 +9,17 @@ Handlebars.registerHelper("formatDate", function (date) {
     return Utils.dateAsDDMMYYYYString(date);
 });
 
-Handlebars.registerHelper('icons', function(importance, block) {
+Handlebars.registerHelper('showStars', function(importance, block) {
     let concatIcons = "";
     for(let i = 0; i <= importance; ++i)
         concatIcons  = concatIcons + "&#9733";
     return concatIcons;
+});
+
+Handlebars.registerHelper ("setChecked", function (value, currentValue) {
+    if ( value == currentValue ) {
+        return "checked";
+    } else {
+        return "";
+    }
 });
